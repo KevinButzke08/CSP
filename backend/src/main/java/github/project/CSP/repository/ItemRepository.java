8fa7fb641e147f86abbe3a0e047c0fd847762e20
Kevin Butzke <kevin.butzke@tu-dortmund.de>
2025-10-26 23:24:00 +0100

    BACKEND: Correcting SQLite implementation, now correctly setting the file and directory; renamed the InventoryRepository to ItemRepository

--- backend/src/main/java/github/project/CSP/repository/ItemRepository.java
+++ backend/src/main/java/github/project/CSP/repository/ItemRepository.java
@@ -3,5 +3,5 @@ package github.project.CSP.repository;
 import github.project.CSP.inventory.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InventoryRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
