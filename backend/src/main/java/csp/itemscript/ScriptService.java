856318b2ab19a57b47890ff531f747c7981890a5
Kevin Butzke <kevin.butzke@tu-dortmund.de>
2026-02-27 13:25:57 +0100

    BACKEND: Rebuilding the ScriptService to use the API endpoint of ByMykel directly and process the item names. Remaining problem is duplicated Enums, where the space is left after removing the condition

--- backend/src/main/java/csp/itemscript/ScriptService.java
+++ backend/src/main/java/csp/itemscript/ScriptService.java
@@ -4,55 +4,45 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestClient;
+import tools.jackson.core.JsonParser;
+import tools.jackson.core.JsonToken;
+import tools.jackson.core.ObjectReadContext;
+import tools.jackson.core.json.JsonFactory;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 @Profile("script")
 public class ScriptService implements CommandLineRunner {
-    private final WebClient webClient;
-    //TODO: Rebuild the ScriptService to take the JSON from ByMykel and extract all names from there
-    public ScriptService(WebClient.Builder builder) {
-        this.webClient = builder.baseUrl("https://steamcommunity.com/market/search/render").build();
+    // Using RestClient, as we need to make one time request, which can also be blocking
+    private final RestClient restClient;
+    private final JsonFactory jsonFactory = new JsonFactory();
+
+    public ScriptService(RestClient.Builder builder) {
+        this.restClient = builder.baseUrl("https://raw.githubusercontent.com/ByMykel/CSGO-API/main/public/api/en/all.json").build();
     }
 
-    public void getItemNames() throws InterruptedException {
+    public void getItemNames() throws Exception {
         Set<String> distinctItemNames = new HashSet<>();
-        for (int i = 0; i < 30051; i += 10) {
-            int finalI = i;
-            TimeUnit.SECONDS.sleep(10);
-            SteamMarketListings result = webClient.get()
-                    .uri(uriBuilder -> uriBuilder
-                            .queryParam("start", finalI)
-                            .queryParam("count", "10")
-                            .queryParam("search_descriptions", "0")
-                            .queryParam("sort_column", "popular")
-                            .queryParam("sort_dir", "desc")
-                            .queryParam("appid", "730")
-                            .queryParam("norender", "1")
-                            .build())
-                    .retrieve()
-                    .bodyToMono(SteamMarketListings.class)
-                    .block();
+        String json = restClient.get().retrieve().body(String.class);
+        JsonParser parser = jsonFactory.createParser(ObjectReadContext.empty(), json);
 
-            for (SteamItem element : result.getResults()) {
-                // 1. Remove StatTrak™, if existent
-                // 2. Replace wear conditions e.g (Factory-New), if existent
-                String clean_market_hash_name = element.getHash_name().replace("StatTrak™", "").replaceAll("\\s*\\([^)]*\\)", "").trim();
-
-                String keyEnum = clean_market_hash_name.toUpperCase().replace(" ", "_");
-
-                String enumEntry = keyEnum.concat("(\"").concat(clean_market_hash_name).concat("\"),");
-
-                distinctItemNames.add(enumEntry);
-                log.info(enumEntry);
+        while (parser.nextToken() != null) {
+            // Search for market_hash_name
+            if (parser.currentToken() == JsonToken.PROPERTY_NAME && parser.currentName().contentEquals("market_hash_name")) {
+                parser.nextToken();
+                // If market_hash_name = null, item cant go on the market so skip it
+                if (parser.getValueAsString() != null) {
+                    String enumEntry = getEnumEntry(parser);
+                    distinctItemNames.add(enumEntry);
+                    log.info(enumEntry);
+                }
             }
         }
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("backend/src/main/java/csp/itemScript/ItemNames.txt"))) {
@@ -65,9 +55,23 @@ public class ScriptService implements CommandLineRunner {
             e.printStackTrace();
         }
     }
+    //TODO: Fix the space left after removing the (holo) of a sticker
+    private static String getEnumEntry(JsonParser parser) {
+        // Remove StatTrak and all Condition parentheses like (Factory New)
+        String valueEnum = parser.getValueAsString().replace("StatTrak™ ", "").replaceAll("\\s*\\([^)]*\\)", "").trim();
+        String keyEnum = valueEnum.toUpperCase()
+                .replaceAll("[^A-Z0-9_]", "_")   // replace any non-alphanumeric/underscore with _
+                .replaceAll("_+", "_")           // collapse multiple underscores
+                .replaceAll("^_|_$", "");        // remove leading/trailing underscores
+        if (Character.isDigit(keyEnum.charAt(0))) {              // E.g "2020 RMR" enum can't start with number
+            keyEnum = "_" + keyEnum;
+        }
+        return keyEnum.concat("(\"").concat(valueEnum).concat("\"),");
+    }
 
     @Override
     public void run(String... args) throws Exception {
         getItemNames();
+        log.info("Successfully loaded all item names!");
     }
 }
