6401cd53b209694343c59c83da71ad470b6577e7
Kevin Butzke <kevin.butzke@tu-dortmund.de>
2026-03-14 23:12:11 +0100

    BACKEND: Removing explizit mapping

--- backend/src/main/java/csp/service/ItemMapper.java
+++ backend/src/main/java/csp/service/ItemMapper.java
@@ -3,10 +3,8 @@ package csp.service;
 import csp.controller.ItemDTO;
 import csp.inventory.Item;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
-    @Mapping(target = "purchasePrice", source = "purchasePrice")
     Item itemDTOtoItem(ItemDTO itemDTO);
 }
